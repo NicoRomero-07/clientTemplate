dc497728bcae345c9638ae49b1933fef085dde18
nicor <nicoromero07@uma.es>
2022-12-16 11:29:22 +0100

    fallo corregido url opendata

--- src/api/FetchOpenData.js
+++ src/api/FetchOpenData.js
@@ -1,6 +1,4 @@
-//const BASE_URL = "http://3.127.55.81:8002";
-//const BASE_URL = "http://localhost:8002";
-const BASE_URL = "https://server-template-api.fly.dev/";
+const API_BASE_URL = "http://3.127.55.81:8003";
 
 const getBusByID = async (id) => {
   const res = await fetch(`http://localhost:8000/buses/${id}`);
